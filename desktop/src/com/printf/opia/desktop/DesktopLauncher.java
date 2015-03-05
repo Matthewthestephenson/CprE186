1c6db6266ff6a89b4430aec3627143ce47c836b7
cod0r <er3bosx@gmail.com>
2015-03-04 23:49:02 -0600

    Configured the "starter classes" (the launchers for all platforms)

--- desktop/src/com/printf/opia/desktop/DesktopLauncher.java
+++ desktop/src/com/printf/opia/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.printf.opia.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Opia";
+        config.width = 1280;
+        config.height = 800;
 		new LwjglApplication(new Game(), config);
 	}
 }
