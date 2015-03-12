f69e8f96049c93ac2cee07abc0ad2e2eec98ddde
deedug1 <jfpachl@iastate.edu>
2015-03-12 15:39:17 -0500

    added display shit...... pretty cool

--- core/src/com/printf/opia/Game.java
+++ core/src/com/printf/opia/Game.java
@@ -1,29 +1,129 @@
 package com.printf.opia;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Game extends ApplicationAdapter {
+public class Game extends ApplicationAdapter implements ApplicationListener, InputProcessor {
 	SpriteBatch batch;
-	Texture img;
+    Camera gameCamera;
+    Sprite currentPiece;
+    Sprite background;
+	Texture redSquare;
+    Texture blueSquare;
+    Texture redCircle;
+    Texture blueCircle;
+    Texture backgroundTexture;
+    int pieceDimensions;
+    static final int GAME_HEIGHT = 1280;
+    static final int GAME_WIDTH = 800;
     protected static Logic gameLogic;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        gameCamera = new OrthographicCamera(GAME_WIDTH,GAME_HEIGHT);
+        gameCamera.translate(gameCamera.viewportWidth/2 ,gameCamera.viewportHeight/2, 0);
+        backgroundTexture = new Texture(Gdx.files.internal("shittyMSPaintPng/background.png"));
+		redSquare = new Texture(Gdx.files.internal("shittyMSPaintPng/redSquare.png"));
+        blueSquare = new Texture(Gdx.files.internal("shittyMSPaintPng/blueSquare.png"));
+        redCircle = new Texture(Gdx.files.internal("shittyMSPaintPng/redCircle.png"));
+        blueCircle = new Texture(Gdx.files.internal("shittyMSPaintPng/blueCircle.png"));
+
+        background = new Sprite(backgroundTexture);
+        background.setSize(GAME_WIDTH,GAME_HEIGHT);
+        pieceDimensions = GAME_WIDTH / 7;
+        currentPiece = new Sprite(redSquare);
+        currentPiece.setSize(pieceDimensions,pieceDimensions);
+
         gameLogic = new Logic();
+
+        Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render () {
+        int i = 0;
+        int j = 0;
+        int counter = 0;
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            gameCamera.translate(-1f,0f,0f);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            gameCamera.translate(1f,0f,0f);
+        }
+        gameCamera.update();
 		batch.begin();
-		batch.draw(img, 0, 0);
+        batch.setProjectionMatrix(gameCamera.combined);
+        background.draw(batch);
+        for(i = 0; i < 6; i++){
+            for(j = 0; j < 7; j++){
+                if(counter % 2 == 0){currentPiece = new Sprite(redCircle);}
+                else{currentPiece = new Sprite(redSquare);}
+                currentPiece.setSize(pieceDimensions,pieceDimensions);
+                currentPiece.setX((j * pieceDimensions));
+                currentPiece.setY(i * pieceDimensions);
+                currentPiece.draw(batch);
+                counter++;
+            }
+        }
 		batch.end();
 	}
+    public void dispose(){
+        redCircle.dispose();
+        redSquare.dispose();
+        blueCircle.dispose();
+        blueSquare.dispose();
+        backgroundTexture.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
--- desktop/src/com/printf/opia/desktop/DesktopLauncher.java
+++ desktop/src/com/printf/opia/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Opia";
-        config.width = 1280;
-        config.height = 800;
+        config.width = 800;
+        config.height = 1280;
 		new LwjglApplication(new Game(), config);
 	}
 }
